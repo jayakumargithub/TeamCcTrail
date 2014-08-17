3c9576161ba21080b5a04e0f0dd3ed98782a0012
jayakumargithub <jayakumar.m.h@gmail.com>
2014-08-17 09:10:38 +0100

    first failed test

--- TeamcityTrail/Bank.Test/Class1.cs
+++ TeamcityTrail/Bank.Test/Class1.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace Bank.Test
 {
     public class Class1
     {
+
+        [Test]
+        public void SomeTest()
+        {
+            var name = "Jayakumar";
+            Assert.AreEqual("Jaya", name);
+        }
     }
 }
